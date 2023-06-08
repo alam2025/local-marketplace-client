80dc633cceab2ceb7e5e8aaa6b5a05ef48717c1d
alam2025 <alam13100852@gmail.com>
2023-06-09 00:13:33 +0600

    classaes api setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
@@ -30,10 +31,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const bannerCollection= client.db('languageSchoolDB').collection('bannerImg')
+    const bannerCollection = client.db('languageSchoolDB').collection('bannerImg')
+    const instructorCollection = client.db('languageSchoolDB').collection('instructors')
+    const classCollection = client.db('languageSchoolDB').collection('classes')
+    const enrollCollection = client.db('languageSchoolDB').collection('enrollCourse')
 
-    app.get('/banners',async(req,res)=>{
-      const result= await bannerCollection.find().toArray()
+
+    // jwt token generate 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ token })
+})
+    app.get('/banners', async (req, res) => {
+      const result = await bannerCollection.find().toArray()
+      res.send(result)
+    })
+    // instructors api 
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result)
+
+    })
+
+    //classes api
+    app.get('/classes', async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    //added course to server
+    app.post('/enrollCourse',async(req,res)=>{
+      const item= req.body;
+      const result=await enrollCollection.insertOne(item);
+      res.json(result)
+    })
+
+    app.get('/enrollCourse',async(req,res)=>{
+      const result = await enrollCollection.find().toArray();
       res.send(result)
     })
 
